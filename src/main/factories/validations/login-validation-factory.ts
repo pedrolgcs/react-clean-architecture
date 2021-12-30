96d7045aafa1a1231d3cd4b008a2ba0f8a3498e2
Pedro Henrique <pedro.lg.cs@gmail.com>
2021-12-30 20:42:51 -0300

    refactor: move yup to infra folder

--- src/main/factories/validations/login-validation-factory.ts
+++ src/main/factories/validations/login-validation-factory.ts
@@ -2,10 +2,10 @@
 import { Validation } from '@/validation/protocols/validation';
 
 // validate
-import { YupValidate } from '@/validation/infra/yup/yup-validate';
+import { YupValidate } from '@/infra/validation/yup/yup-validate';
 
 // schema
-import { loginSchema } from '@/validation/infra/yup/schemas/login-schema';
+import { loginSchema } from '@/infra/validation/yup/schemas/login-schema';
 
 const makeLoginValidation = (): Validation => {
   return new YupValidate(loginSchema);

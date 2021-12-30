9afb1d5cc866fb7ccb556859d30c951c71760c1d
Pedro Henrique <pedro.lg.cs@gmail.com>
2021-12-30 12:05:50 -0300

    refactor: move SUT creation to a factory method

--- src/presentation/pages/Login/login.spec.tsx
+++ src/presentation/pages/Login/login.spec.tsx
@@ -1,16 +1,27 @@
-import { render, screen } from '@testing-library/react';
+import { render, RenderResult } from '@testing-library/react';
 
 // component
 import { Login } from './index';
 
+type SutTypes = {
+  sut: RenderResult;
+};
+
+const makeSut = (): SutTypes => {
+  const sut = render(<Login />);
+  return {
+    sut,
+  };
+};
+
 describe('Login', () => {
   it('renders correctly', () => {
-    render(<Login />);
-    expect(screen.getByText(/login/i)).toBeInTheDocument();
+    const { sut } = makeSut();
+    expect(sut.getByText(/login/i)).toBeInTheDocument();
   });
 
   it('should not render spinner on start', () => {
-    render(<Login />);
-    expect(screen.getByTestId('spinner-wrap').childElementCount).toBe(0);
+    const { sut } = makeSut();
+    expect(sut.getByTestId('spinner-wrap').childElementCount).toBe(0);
   });
 });

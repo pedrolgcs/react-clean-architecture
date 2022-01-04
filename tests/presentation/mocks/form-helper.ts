5105eb1397debc9277d8f717dd4425be46372ba1
Pedro Henrique <pedro.lg.cs@gmail.com>
2022-01-04 10:50:15 -0300

    test: creare form-helper

---
+++ tests/presentation/mocks/form-helper.ts
@@ -0,0 +1,10 @@
+import { fireEvent, screen } from '@testing-library/react';
+import faker from 'faker';
+
+export const populateField = (
+  fieldName: string,
+  value = faker.random.word(),
+): void => {
+  const input = screen.getByTestId(fieldName);
+  fireEvent.input(input, { target: { value } });
+};
